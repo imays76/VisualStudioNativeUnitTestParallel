6b35c3f75a458aebc0e8ed1c9e33371dd09c6acf
Hyunjik Bae <imays76@gmail.com>
2018-08-12 18:41:43 +0900

    1st

---
+++ UnitTest1/unittest1.cpp
@@ -0,0 +1,26 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include <windows.h>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+			Sleep(5000);
+		}
+		TEST_METHOD(TestMethod2)
+		{
+			// TODO: Your test code here
+			Sleep(5000);
+		}
+
+	};
+
+}
